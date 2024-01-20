b1b094664481564cf792f32d08ef55a51aa172db
ReDxDaGer <yashmehrotra45@gmail.com>
2024-01-20 22:36:37 +0530

    Updated the image for the login page

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -16,11 +16,12 @@ const Login: React.FC<Props> = ({navigation:{navigate}}) =>{
         <SafeAreaView>
             <View style={{padding:20}}>
                 <View style={{alignItems:'center'}}>
-                    <Image resizeMode="contain" source={require('../../assets/firebase2.png')}
+                    <Image resizeMode="contain" source={require('../../assets/Vetovet.png')}
                     style={{width:120,height:120}}
                     />
                 </View>
-                <Text style={{marginHorizontal:78,fontSize:30,color:'#f6880e',marginVertical:25,fontWeight:'bold',width:300}}>Login Here</Text>
+                
+		<Text style={{marginHorizontal:25 , fontSize:35 , color:'#f6880e',marginVertical:5,fontWeight:'bold',width:300}}>Login to Veto Vets</Text>
             </View>
 	    <View style={{paddingVertical:30}}>
 		<AppInput placeholder="Email" />
