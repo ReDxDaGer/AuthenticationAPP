512c5ec77753dc47a42d096fa7515ac2e9c156c0
ReDxDaGer <yashmehrotra45@gmail.com>
2024-01-21 13:06:01 +0530

    Solved the erro(Updated the imports and images)

--- src/screens/signup.tsx
+++ src/screens/signup.tsx
@@ -1,6 +1,6 @@
-import { Text } from "@rneui/themed";
 import React from "react";
-import { SafeAreaView,View } from "react-native";
+import { SafeAreaView,View, TouchableOpacity,Text,Image } from "react-native";
+import AppInput from "../components/appinput";
 interface Props {
     navigation: {
       navigate: (screen: string) => void;
@@ -17,17 +17,17 @@ const Signup: React.FC<Props> = ({navigation:{navigate}}) =>{
                     />
                 </View>
                 
-		<Text style={{marginHorizontal:25 , fontSize:35 , color:'#f6880e',marginVertical:5,fontWeight:'bold',width:300}}>SignUp to Veto Vets</Text>
+		<Text style={{marginHorizontal:50 , fontSize:25 , color:'#f6880e',marginVertical:5,fontWeight:'bold',width:300}}>SignUp to Veto Vets</Text>
             </View>
 	    <View style={{paddingVertical:30}}>
-	    	<AppInput placeholder="Name"/>
+	    <AppInput placeholder="Name"/>
 		<AppInput placeholder="Email" />
 		<AppInput placeholder="Password" />
 	    </View>
-	    <TouchableOpacity onPress={()=>{}} style={{padding:20,marginVertical:10,borderRadius:10,backgroundcolor:"#f6880e",shadowOffset:{{width:0,height:10,shadowOpacity: 0,shadowRadius: 10,}}}}>
+	    <TouchableOpacity onPress={()=>{}} style={{padding:20,marginVertical:10,borderRadius:10,backgroundColor:"#f6880e"}}>
 	    	<Text style={{color:"#222",textAlign:"center",fontSize:20}}>Sign Up</Text>
 	    </TouchableOpacity>
-	    <TouchableOpacity onPress={()=>{navigate("Login")}} style={{padding:20,marginVertical:30,}}>
+	    <TouchableOpacity onPress={()=>{navigate("Login")}} style={{padding:20,marginVertical:5,}}>
 	    	<Text style={{color:'#222',fontSize:20,textAlign:'center'}}>Already have a Account</Text>
 	    </TouchableOpacity>
         </SafeAreaView>
